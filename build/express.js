8bc9c08250ae54f4a3c68d292b8f14c791d481c1
Daniel Lao <dlao@emailvision.com>
2015-12-17 14:00:59 +0100

    heroku config

--- build/express.js
+++ build/express.js
@@ -1,10 +1,9 @@
-/*eslint-disable */
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(request, response) {
   response.render('index.html');
@@ -13,4 +12,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-/*eslint-enable */
